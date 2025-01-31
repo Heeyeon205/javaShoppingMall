f895455a23b41d64b767578e196f3541ceb117b2
Lyle <procross27@gmail.com>
2025-01-31 16:28:41 +0900

    feat

--- Level 10 Shopping Mall/src/dto/Cart.java
+++ Level 10 Shopping Mall/src/dto/Cart.java
@@ -6,7 +6,7 @@ public class Cart {
 	private String id;
 	private int itemNum;
 	private int itemCnt;
-	
+
 	public Cart(String id, int itemNum, int itemCnt) {
 		this.cartNum = ++num;
 		this.id = id;
@@ -22,30 +22,39 @@ public class Cart {
 	public static int getNum() {
 		return num;
 	}
+
 	public static void setNum(int num) {
 		Cart.num = num;
 	}
+
 	public int getCartNum() {
 		return cartNum;
 	}
+
 	public void setCartNum(int cartNum) {
 		this.cartNum = cartNum;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public int getItemNum() {
 		return itemNum;
 	}
+
 	public void setItemNum(int itemNum) {
 		this.itemNum = itemNum;
 	}
+
 	public int getItemCnt() {
 		return itemCnt;
 	}
+
 	public void setItemCnt(int itemCnt) {
 		this.itemCnt = itemCnt;
 	}
